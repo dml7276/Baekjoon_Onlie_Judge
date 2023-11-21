2f04ba8fd20af1872a0a670bc3dcf0260a5dbaa9
dml7276 <49231342+dml7276@users.noreply.github.com>
2023-11-21 12:11:39 +0900

    feat: 오늘 날짜

---
+++ src/Bronze/P10699.java
@@ -0,0 +1,11 @@
+package Bronze;
+
+import java.time.LocalDate;
+
+public class P10699 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+
+}
