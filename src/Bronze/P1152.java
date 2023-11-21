638a4ffd7fbb391dccfa0e4ac0f571ce6939838f
dml7276 <49231342+dml7276@users.noreply.github.com>
2023-11-21 11:40:03 +0900

    feat: 단어의 개수

---
+++ src/Bronze/P1152.java
@@ -0,0 +1,18 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P1152 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String ex = sc.nextLine().strip();
+        String[] array = ex.split(" ");
+
+        if(!ex.isEmpty()) {
+            System.out.print(array.length);
+        }
+        else {
+            System.out.print(0);
+        }
+    }
+}
