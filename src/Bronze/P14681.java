9e61d2063cbb2b7d26a8bbafda38057f1bb0fb4c
dml7276 <49231342+dml7276@users.noreply.github.com>
2023-11-23 19:06:16 +0900

    feat: 사분면 고르기

---
+++ src/Bronze/P14681.java
@@ -0,0 +1,27 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a > 0 && b > 0) {
+            System.out.println("1");
+        }
+
+        else if(a < 0 && b > 0) {
+            System.out.println("2");
+        }
+
+        else if(a < 0 && b < 0) {
+            System.out.println("3");
+        }
+
+        else if(a > 0 && b < 0) {
+            System.out.println("4");
+        }
+    }
+}
