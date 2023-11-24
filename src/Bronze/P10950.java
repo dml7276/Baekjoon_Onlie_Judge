a48ccfc342177183de2fd819d4b5c2640f225d99
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-24 19:55:20 +0900

    feat: A+B - 3

---
+++ src/Bronze/P10950.java
@@ -0,0 +1,20 @@
+package Bronze;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P10950 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		ArrayList<Integer> integers = new ArrayList<>();
+
+		for (int i = 0; i < t; i++) {
+			integers.add(scanner.nextInt() + scanner.nextInt());
+		}
+
+		for (Integer integer : integers) {
+			System.out.println(integer);
+		}
+	}
+}
