4f9f1896470e8aee4634596c03050fbcc89cdfae
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-24 14:31:56 +0900

    feat: 사파리월드

---
+++ src/Bronze/P2420.java
@@ -0,0 +1,14 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P2420 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        long sum = Math.abs(n - m);
+
+        System.out.println(sum);
+    }
+}
