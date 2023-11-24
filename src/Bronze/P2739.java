46430191ad7891f656ac621bb3960bc0454e7c5a
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-24 20:23:45 +0900

    feat: 구구단

---
+++ src/Bronze/P2739.java
@@ -0,0 +1,14 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P2739 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+
+		for (int i = 1; i < 10; i++) {
+			System.out.println(n + " * " + i + " = " + n * i);
+		}
+	}
+}
