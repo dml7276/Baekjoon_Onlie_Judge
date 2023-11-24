9edd0102e0a8927247269721722e3c0b34af4f74
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-24 15:52:00 +0900

    feat: N 찍기

---
+++ src/Bronze/P2741.java
@@ -0,0 +1,14 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P2741 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i=1;i<=n;i++) {
+          System.out.println(i);
+        }
+    }
+}
