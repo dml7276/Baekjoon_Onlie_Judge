95b39f2fefa385c941e602dd7acce049f38ae1c8
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-24 16:07:54 +0900

    feat: 팩토리얼

---
+++ src/Bronze/P10872.java
@@ -0,0 +1,17 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P10872 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int sum = 1;
+
+		for (int i = 1; i <= n; i++) {
+			sum *= i;
+		}
+
+		System.out.println(sum);
+	}
+}
