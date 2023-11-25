ad3b92002c18c799886fe07460faa96308dae62e
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-25 22:43:38 +0900

    feat: A+B - 4

---
+++ src/Bronze/P10951.java
@@ -0,0 +1,15 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P10951 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		while (scanner.hasNext()) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			System.out.println(a + b);
+		}
+	}
+}
