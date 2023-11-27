6011635dcfb1c33ed9227731d52a5871a49cc66c
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-27 18:56:52 +0900

    feat: 단어 길이 재기

---
+++ src/Bronze/P2743.java
@@ -0,0 +1,12 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P2743 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+
+		System.out.println(str.length());
+	}
+}
