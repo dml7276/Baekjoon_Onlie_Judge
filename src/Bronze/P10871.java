cb674e06800847de5c2ae188935a25be314be15b
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-27 15:42:23 +0900

    feat: X보다 작은 수

---
+++ src/Bronze/P10871.java
@@ -0,0 +1,21 @@
+package Bronze;
+
+import java.util.Scanner;
+public class P10871 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		int[] temp = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			temp[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (x > temp[i]) {
+				System.out.print(temp[i] + " ");
+			}
+		}
+	}
+}
