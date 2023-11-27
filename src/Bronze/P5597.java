485eccc6f99a0c9831e455adeff154b570d2ca67
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-27 18:28:34 +0900

    feat: 과제 안 내신 분..?

---
+++ src/Bronze/P5597.java
@@ -0,0 +1,21 @@
+package Bronze;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P5597 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Integer> arrayList = new ArrayList<>();
+
+		for (int i = 0; i < 28; i++) {
+			arrayList.add(scanner.nextInt());
+		}
+
+		for (int i = 1; i <= 30; i++) {
+			if (!arrayList.contains(i)){
+				System.out.println(i);
+			}
+		}
+	}
+}
