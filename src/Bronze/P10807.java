a307e893815a631c8e5a86d358f228c13f944f91
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-27 15:49:46 +0900

    feat: 개수 세기

---
+++ src/Bronze/P10807.java
@@ -0,0 +1,26 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P10807 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] array = new int[n];
+		int count = 0;
+
+		for (int i = 0; i < n; i++) {
+			array[i] = scanner.nextInt();
+		}
+
+		int x = scanner.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			if (array[i] == x){
+				count++;
+			}
+		}
+
+		System.out.println(count);
+	}
+}
