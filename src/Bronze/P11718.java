8d8ea10f9de571bc00f02987e355dd1730140015
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-28 22:09:14 +0900

    feat: 그대로 출력하기

---
+++ src/Bronze/P11718.java
@@ -0,0 +1,13 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P11718 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNextLine()) {
+			String string = scanner.nextLine().strip();
+			System.out.println(string);
+		}
+	}
+}
