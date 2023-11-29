ee6c73832338a796f6ea6ba7feef0f197cc60611
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-29 18:37:32 +0900

    feat: 이상한 기호

---
+++ src/Bronze/P15964.java
@@ -0,0 +1,18 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class P15964 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		long a = scanner.nextLong();
+		long b = scanner.nextLong();
+
+		System.out.println(NewOperation(a, b));
+	}
+
+	public static long NewOperation(long A, long B) {
+		return (A + B) * (A - B);
+	}
+}
