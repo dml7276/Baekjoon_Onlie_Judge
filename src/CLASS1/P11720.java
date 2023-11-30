cb08c8ec67f429a9de60367bf99fb13fcb206199
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-30 21:12:09 +0900

    feat: 숫자의 합

---
+++ src/CLASS1/P11720.java
@@ -0,0 +1,24 @@
+package CLASS1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P11720 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int sum = 0;
+		String[] str;
+		str = scanner.next().split("");
+		ArrayList<Integer> arrayList = new ArrayList<>();
+
+		for (String string : str) {
+			arrayList.add(Integer.parseInt(string));
+		}
+
+		for (Integer integer : arrayList) {
+			sum += integer;
+		}
+
+		System.out.println(sum);
+	}
+}
