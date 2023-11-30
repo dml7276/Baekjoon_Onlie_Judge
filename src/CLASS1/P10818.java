c149174ae6c8bf290a609de81689b9bafd435d14
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-11-30 19:50:56 +0900

    feat: 최소, 최대

---
+++ src/CLASS1/P10818.java
@@ -0,0 +1,21 @@
+package CLASS1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P10818 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] intArray = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			intArray[i] = scanner.nextInt();
+		}
+
+		int max = Arrays.stream(intArray).max().orElse(0);
+		int min = Arrays.stream(intArray).min().orElse(0);
+
+		System.out.println(min + " " + max);
+	}
+}
