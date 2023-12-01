202b5910312863552598a7406a01ce888f99eb97
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-12-01 12:49:25 +0900

    feat: 알람 시계

---
+++ src/CLASS1/P2884.java
@@ -0,0 +1,25 @@
+package CLASS1;
+
+import java.util.Scanner;
+
+public class P2884 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int h = scanner.nextInt();
+		int m = scanner.nextInt();
+
+		if (m >= 45) {
+			m = m - 45;
+		}
+		else {
+			h--;
+			m = 15 + m;
+		}
+
+		if (h == -1) {
+			h = 23;
+		}
+
+		System.out.println(h + " " + m);
+	}
+}
