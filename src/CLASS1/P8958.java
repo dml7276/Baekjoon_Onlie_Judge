429479bc9d64c382df163586e771ca20793a84ea
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-12-02 11:26:15 +0900

    feat: OX퀴즈

---
+++ src/CLASS1/P8958.java
@@ -0,0 +1,32 @@
+package CLASS1;
+
+import java.util.Scanner;
+
+public class P8958 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int test = scanner.nextInt();
+		int[] result = new int[test];
+
+		for (int i = 0; i < test; i++) {
+			String[] strings = scanner.next().split("");
+			int count = 0;
+			int score = 0;
+
+			for (String s : strings) {
+				if (s.equals("O")) {
+					count++;
+					score += count;
+				}
+				else {
+					count = 0;
+				}
+			}
+			result[i] = score;
+		}
+
+		for (Integer i : result) {
+			System.out.println(i);
+		}
+	}
+}
