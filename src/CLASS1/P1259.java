786d44c3b36990823f34ab1df133ebf0d4098588
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-12-08 16:24:18 +0900

    feat: 팰린드롬수

---
+++ src/CLASS1/P1259.java
@@ -0,0 +1,39 @@
+package CLASS1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P1259 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 1;
+		int mid;
+		int last;
+		String temp;
+		String[] strings;
+		ArrayList<String> resultArray = new ArrayList<>();
+
+		while (num != 0) {
+			String result = "yes";
+			num = sc.nextInt();
+			temp = String.valueOf(num);
+			last = temp.length() - 1;
+			mid = temp.length() / 2;
+			strings = temp.split("");
+
+			for (int i = 0; i < mid; i++) {
+				if (!strings[i].equals(strings[last - i])) {
+					result = "no";
+					break;
+				}
+			}
+			if (num != 0) {
+				resultArray.add(result);
+			}
+		}
+
+		for (String s : resultArray) {
+			System.out.println(s);
+		}
+	}
+}
