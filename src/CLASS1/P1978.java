06e2bb0349e5adbd6b2e1841c4185500ad3f6782
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-12-09 10:48:16 +0900

    feat: 소수 찾기

---
+++ src/CLASS1/P1978.java
@@ -0,0 +1,30 @@
+package CLASS1;
+
+import java.util.Scanner;
+
+public class P1978 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] num = new int[n];
+		int count = 0;
+		int result = 0;
+
+		for (int i = 0; i < n; i++) {
+			num[i] = scanner.nextInt();
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j <= num[i]; j++) {
+				if (num[i] % j == 0) {
+					count++;
+				}
+			}
+			if (count == 2) {
+				result++;
+			}
+			count = 0;
+		}
+		System.out.println(result);
+	}
+}
