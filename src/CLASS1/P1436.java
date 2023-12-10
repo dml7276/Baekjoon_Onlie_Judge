a7a260ffecf6cc0213c7c0aa0aa2719dd7f75049
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-12-10 12:40:19 +0900

    feat: 영화감독 숌

---
+++ src/CLASS1/P1436.java
@@ -0,0 +1,27 @@
+package CLASS1;
+
+import java.util.Scanner;
+
+public class P1436 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int countN = 0;
+		int result = 0;
+		int i = 0;
+
+		while (countN != n) {
+			i++;
+			String str = String.valueOf(i);
+
+			if (str.contains("666")) {
+				countN++;
+			}
+
+			if (countN == n) {
+				result = i;
+			}
+		}
+		System.out.println(result);
+	}
+}
