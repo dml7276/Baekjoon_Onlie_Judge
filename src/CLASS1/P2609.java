9fdecddf35d47436ab30706c4b1081d17825e0df
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-12-11 21:44:13 +0900

    feat: 최대공약수와 최소공배수

---
+++ src/CLASS1/P2609.java
@@ -0,0 +1,31 @@
+package CLASS1;
+
+import java.util.Scanner;
+
+public class P2609 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num1 = scanner.nextInt();
+		int num2 = scanner.nextInt();
+		int max = num1 * num2;
+		int min = Math.min(num1, num2);
+		int lcm = 0, gcd = 0;
+
+		for (int i = 1; i <= max; i++) {
+			if (i % num1 == 0 && i % num2 == 0) {
+				lcm = i;
+				break;
+			}
+		}
+
+		for (int i = min; i >= 1; i--) {
+			if (num1 % i == 0 && num2 % i == 0) {
+				gcd = i;
+				break;
+			}
+		}
+
+		System.out.println(gcd);
+		System.out.println(lcm);
+	}
+}
