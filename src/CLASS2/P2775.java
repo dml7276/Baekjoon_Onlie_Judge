893e1d32d0ccf5b1e402df09c12d21dc72447fce
Wongi Jeong / 정원기 <dml7276@gmail.com>
2023-12-16 21:14:12 +0900

    feat: 달팽이는 올라가고 싶다

---
+++ src/CLASS2/P2775.java
@@ -0,0 +1,24 @@
+package CLASS2;
+
+import java.util.Scanner;
+
+public class P2775 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long dayTime = sc.nextInt();
+		long sleepTime = sc.nextInt();
+		long goals = sc.nextInt();
+		long cumulativeDay;
+		long currentPoint;
+
+		cumulativeDay = (goals - sleepTime) / (dayTime - sleepTime);
+		currentPoint = sleepTime + (dayTime - sleepTime) * cumulativeDay;
+
+		if (currentPoint < goals) {
+			cumulativeDay++;
+		}
+
+		System.out.println(cumulativeDay);
+
+	}
+}
