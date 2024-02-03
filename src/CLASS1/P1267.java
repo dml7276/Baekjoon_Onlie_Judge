b44e254d8179f7c0499a326fe131e3b2ae99b010
정원기 <dml7276@gmail.com>
2024-02-03 22:46:27 +0900

    feat: 핸드폰 요금

---
+++ src/CLASS1/P1267.java
@@ -0,0 +1,27 @@
+package src.CLASS1;
+
+import java.util.Scanner;
+
+public class P1267 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K;
+        int YoungSik = 0, MinSik = 0;
+        for (int i = 0; i < N; i++) {
+            K = sc.nextInt();
+            YoungSik += ((K / 30) + 1) * 10;
+            MinSik += ((K / 60) + 1) * 15;
+
+        }
+        if (YoungSik == MinSik) {
+            System.out.println("Y M " + YoungSik);
+        } else if (YoungSik < MinSik) {
+            System.out.println("Y " + YoungSik);
+        } else {
+            System.out.println("M " + MinSik);
+        }
+    }
+}
+
