f24627e6ab53d3327ee98bde38282f1bf3d164dc
정원기 <dml7276@gmail.com>
2024-02-06 22:28:58 +0900

    feat: 그대로 출력하기 2

---
+++ src/CLASS1/P11719.java
@@ -0,0 +1,16 @@
+package src.CLASS1;
+
+
+import java.util.Scanner;
+
+public class P11719 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+
+        scanner.close();
+    }
+}
