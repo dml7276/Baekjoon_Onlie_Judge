6a08ba7d8f3990e4b0fb0b940f607a2b091be6f6
정원기 <dml7276@gmail.com>
2024-02-18 21:22:24 +0900

    feat: 꼬리를 무는 숫자 나열

---
+++ src/CLASS1/P1598.java
@@ -0,0 +1,19 @@
+package src.CLASS1;
+
+import java.util.Scanner;
+
+public class P1598 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt() - 1;
+        int b = scanner.nextInt() - 1;
+
+        int answer = 0;
+
+        answer += Math.abs(a / 4 - b / 4);
+        answer += Math.abs(a % 4 - b % 4);
+
+        System.out.println(answer);
+    }
+}
