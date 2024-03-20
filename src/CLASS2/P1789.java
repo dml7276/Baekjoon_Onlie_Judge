37956071d2202f9207923893ae00565414ae889e
Jeong Wongi <dml7276@gmail.com>
2024-03-20 19:40:06 +0900

    feat: 수들의 합

---
+++ src/CLASS2/P1789.java
@@ -0,0 +1,22 @@
+package src.CLASS2;
+
+import java.util.Scanner;
+
+public class P1789 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long num = scan.nextLong();
+        long sum = 0;
+        int count = 0;
+
+        for (int i = 1; ; i++) {
+            if (sum > num) break;
+            sum += i;
+            count++;
+        }
+        System.out.println(count - 1);
+        scan.close();
+    }
+
+}
